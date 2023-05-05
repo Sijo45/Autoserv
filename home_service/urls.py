ce1b63672e1632da135359b8866d95be0e1bc29d
sijomon <sijomons48@gmail.com>
2023-05-05 09:31:17 +0530

    1st

---
+++ home_service/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from home_service.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    
+]
